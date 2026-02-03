ad7046020b49c5658e056e9566ce98fe2bf36ff4
Bhumika Kewate <kewatebhumika5@gmail.com>
2026-02-03 21:15:04 +0530

    Create tronic_arrayI.java

---
+++ tronic_arrayI.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        if (n < 3) return false;
+
+        int i = 0;
+
+        
+        while (i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
+        }
+        int p = i;
+        if (p == 0) return false;
+
+        while (i + 1 < n && nums[i] > nums[i + 1]) {
+            i++;
+        }
+        int q = i;
+        if (q == p || q == n - 1) return false; 
+        while (i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
+        }
+        return i == n - 1;
+    }
+}
