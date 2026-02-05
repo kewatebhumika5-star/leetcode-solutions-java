90a38bd3792729ad5e85cb88c498a599ee7f9a8f
Bhumika Kewate <kewatebhumika5@gmail.com>
2026-02-05 19:59:10 +0530

    Create transformed_array.java

---
+++ transformed_array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int move = nums[i];
+
+            if (move == 0) {
+                result[i] = 0;
+            } else {
+                int newIndex = (i + move) % n;
+
+                // Fix negative index for circular wrap
+                if (newIndex < 0) {
+                    newIndex += n;
+                }
+
+                result[i] = nums[newIndex];
+            }
+        }
+
+        return result;
+    }
+}
