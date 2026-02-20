4420ec70e689be7bfff5a782e849dd75de2ff637
Bhumika Kewate <kewatebhumika5@gmail.com>
2026-02-20 18:11:25 +0530

    Create string-compare.java

---
+++ string-compare.java
@@ -0,0 +1,18 @@
+String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+
+        // Check all substrings of length k
+        for (int i = 1; i <= s.length() - k; i++) {
+            String current = s.substring(i, i + k);
+
+            if (current.compareTo(smallest) < 0) {
+                smallest = current;
+            }
+
+            if (current.compareTo(largest) > 0) {
+                largest = current;
+            }
+        }
+
+        return smallest + "\n" + largest;
+    }
