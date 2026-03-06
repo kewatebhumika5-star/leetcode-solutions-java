74d20ae68f0d1f84b2de99c54ca650142cf448d0
Bhumika Kewate <kewatebhumika5@gmail.com>
2026-03-06 21:05:59 +0530

    Create check-if-binary-string-has-at-most-one-segment-of-ones.java

---
+++ check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        boolean zeroFound = false;
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '0'){
+                zeroFound = true;
+            }
+            if(zeroFound && s.charAt(i) == '1'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
